e96b0644d507a866b78171cbc06152d1a8d34544
Maxim Berestovenko <maximus7970@gmail.com>
2024-04-04 16:49:30 +0300

    Divide

--- GitApp/GitApp.cpp
+++ GitApp/GitApp.cpp
@@ -9,6 +9,18 @@ double subtract(double a, double b)
 {
 	return a - b;
 }
+double divide(double a, double b) 
+{
+	if (b != 0) 
+	{
+		return a / b;
+	}
+	else 
+	{
+		cerr << "Помилка: Ділення на нуль!" << endl;
+		return 0; 
+	}
+}
 
 
 int main()
